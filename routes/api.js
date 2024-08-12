f7b31768aedaf23c60225b5e3429526d6d5834ee
sebahsr <sebahsr@gmail.com>
2024-08-12 10:28:59 +0000

    init

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,44 @@
 'use strict';
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const crypto = require('crypto');
+
+const anonymizeIp = (ip) => {
+  return crypto.createHash('sha256').update(ip).digest('hex');
+};
+const getStockData = async (symbol) => {
+  try {
+    const response = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`);
+    console.log(response.data)
+    const price = response.data.latestPrice;
+    return { stock: symbol, price };
+  } catch (error) {
+    return { error: 'Stock data not found',erro:error};
+  }
+};
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async function  (req, res){
+      const { stock, like } = req.query;
+      const ip = anonymizeIp(req.ip);
+ 
+
+  if (Array.isArray(stock)) {
+    let stockData= await stock.map(symbol => {console.log(symbol)
+      return getStockData(symbol)})
+    const rel_likes = Math.abs(stockData[0].likes - stockData[1].likes);
+    console.log(stockData, rel_likes,"sebah 11")
+    stockData.forEach(data => data.rel_likes = rel_likes);
+    res.json({ stockData });
+  } else {
+    console.log(stock)
+    const stockData = await getStockData(stock);
+    console.log(stockData)
+    res.json({ stockData:{...stockData,likes:like?1:-1  }});
+  }
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,78 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
-const assert = chai.assert;
 const server = require('../server');
+const { expect } = chai;
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+describe('Functional Tests', () => {
+  it('Viewing one stock: GET request to /api/stock-prices/', (done) => {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.stockData).to.be.an('object');
+        expect(res.body.stockData).to.have.property('stock');
+        expect(res.body.stockData).to.have.property('price');
+        expect(res.body.stockData).to.have.property('likes');
+        done();
+      });
+  });
 
+  it('Viewing one stock and liking it: GET request to /api/stock-prices/', (done) => {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG', like: true })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.stockData).to.be.an('object');
+        expect(res.body.stockData).to.have.property('stock');
+        expect(res.body.stockData).to.have.property('price');
+        expect(res.body.stockData).to.have.property('likes');
+        done();
+      });
+  });
+
+  it('Viewing the same stock and liking it again: GET request to /api/stock-prices/', (done) => {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG', like: true })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.stockData).to.be.an('object');
+        expect(res.body.stockData).to.have.property('stock');
+        expect(res.body.stockData).to.have.property('price');
+        expect(res.body.stockData).to.have.property('likes');
+        done();
+      });
+  });
+
+  it('Viewing two stocks: GET request to /api/stock-prices/', (done) => {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'] })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.stockData).to.be.an('array');
+        expect(res.body.stockData[0]).to.have.property('stock');
+        expect(res.body.stockData[1]).to.have.property('stock');
+        done();
+      });
+  });
+
+  it('Viewing two stocks and liking them: GET request to /api/stock-prices/', (done) => {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'], like: true })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.stockData).to.be.an('array');
+        expect(res.body.stockData[0]).to.have.property('stock');
+        expect(res.body.stockData[1]).to.have.property('stock');
+        expect(res.body.stockData[0]).to.have.property('rel_likes');
+        expect(res.body.stockData[1]).to.have.property('rel_likes');
+        done();
+      });
+  });
 });
